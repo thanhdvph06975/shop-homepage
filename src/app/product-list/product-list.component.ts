785c9e0cd4279cb7760d214e6b4e53fd18768918
thanhdvph06975 <demonstyle30@gmail.com>
2020-03-16 15:02:40 +0700

    2 files modified, 1 added

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import {product} from "../product"
 @Component({
-  selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  selector: "app-product-list",
+  templateUrl: "./product-list.component.html",
+  styleUrls: ["./product-list.component.css"]
 })
 export class ProductListComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
+  products: product[] = [
+  {name: "Item 1",price: 24,desc: "good product 1",status: true},
+  {name: "Item 1",price: 24,desc: "good product 1",status: true}
+  ]
 }
